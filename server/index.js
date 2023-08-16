1ae29c15ca137d0d4a3dcf535732972df5f8828f
Jason <jhung2317@gmail.com>
2023-08-16 22:11:38 +0100

    add server folder

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+// import connectDB from './mongodb/connect.js';
+// import postRoutes from './routes/postRoutes.js';
+// import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config();
