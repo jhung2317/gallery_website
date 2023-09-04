b4b5449a6f102db7aae045a0425181e2cad75f74
Jason <jhung2317@gmail.com>
2023-09-04 22:02:45 +0100

    connect to cloudinary and show post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -23,12 +23,64 @@ const CreatePost = () => {
   };
   
   const generateImage = async () => {
-    
+    if(form.prompt){
+      try{
+        setGeneratingImg(true)
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        })
+        
+        const data = await response.json();
+        
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` })
+      } catch (error){
+        if (error.response) {
+          console.log(error.response.status);
+          console.log(error.response.data);
+          alert(error.response.data);
+        } else {
+          console.log(error.message);
+          alert("Sorry! All tokens ran out");
+        }
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please enter a prompt')
+    }
+
   }
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  }
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,8 +14,47 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState('');
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
   
-  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('url', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse()); //show the latest post on the top
+      }
+      } catch(error) {
+        alert(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500),
+    );
+  };
+
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -26,7 +65,14 @@ const Home = () => {
       </div>
 
       <div className='mt-16'>
-        <FormField />
+      <FormField
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search something..."
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
       <div className="mt-10">
         {loading ? (
@@ -43,12 +89,12 @@ const Home = () => {
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
             {searchText ? (
               <RenderCards
-              data={[]}
+              data={searchedResults}
               title="No Search results found" 
               />
             ) : (
               <RenderCards
-              data={[]}
+              data={allPosts}
               title="No posts found" 
               />
             )}
